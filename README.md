fccc1a665d0b7e66b89cd796921f5a378cd52d64
Shini31 <github@capitoul.net>
2018-12-22 18:08:01 +0100

    migrate to acme.sh

--- README.md
+++ README.md
@@ -2,28 +2,26 @@ acme-tiny-wrapper
 =================
 
 
-#Review
-Bash script using acme-tiny for renew let's encrypt certificates
+# Review
+Bash script using acme.sh for renewing let's encrypt certificates
 
-#Version
-1.0
+# Version
+2.0
 
-#Requirements
-* Python
-* [acme-tiny](https://github.com/diafygi/acme-tiny)
+# Requirements
+* Bash
+* [acme.sh](https://github.com/Neilpang/acme.sh)
 
-#Installation
-* Clone the repository (`git clone https://github.com/Shini31/acme-tiny-wrapper`) or download and unpack the archives
-* Modify [renew_certificates.sh](renew_certificates.sh) with correct directories
+# Installation
+* Clone the repository (`git clone https://github.com/Shini31/acme-wrapper`) or download and unpack the archives
+* Modify [renew_certificates.sh](renew_certificates.sh) with correct directories and domain name
 * Create a cron job: `* * 1 * *  /path/to/renew_certificate.sh`
 
-#Configuration
-Change these following variables according to your acme-tiny installation:
+# Configuration
+Change these following variables according to your certificates, acme.sh installation and web server:
 * `HTTPSERVER`: Name of your HTTP web server
-* `PYTHONDIR`: Path to python binary
-* `ACMEDIR`: Path to acme-tiny binary
-* `ACMETINY`: Name of acme-tiny binary
-* `ACMECHALLENGESDIR`: Path to challenges directory
+* `ACMEDIR`: Path to acme.sh binary
+* `ACMETINY`: Name of acme.sh binary
 * `ACMEDOMAINDIR`: Path to domains directory
 * `ACMESECRETSDIR`: Path to Let's Encrypt account private key
 * `ACCOUNTKEY`: Name of Let's Encrypt account private key
--- renew_certificate.sh
+++ renew_certificate.sh
@@ -3,6 +3,7 @@
 # Version   Date       Author    Description
 #------------------------------------------------
 # 1.0       27/03/16   Shini31   Initial version
+# 2.0       22/12/18   Shini31   Migrate to acme.sh
 #
 ###
 
@@ -12,38 +13,18 @@ TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
 
 HTTPSERVER=apache2
 
-PYTHONDIR=/usr/bin/
-
-ACMEDIR=/home/acme/
-ACMETINY=acme_tiny.py
-ACMECHALLENGESDIR=/home/acme/challenges
-ACMEDOMAINDIR=/home/acme/domain
-ACMESECRETSDIR=/home/acme/secrets
-ACCOUNTKEY=account.key
+ACMEDIR=/home/user/.acme.sh
+ACME=acme.sh
 DOMAINS=(domain1 domain2 domain3)
 
 #Check variables
-[ -f $ACMEDIR/$ACMETINY ] || { echo -e "${TIMESTAMP} - Binary acme-tiny not found" > ${LOGFILE}; exit 1; }
-[ -f $ACMESECRETSDIR/$ACCOUNTKEY ] || { echo -e "${TIMESTAMP} - Account key not found" > ${LOGFILE}; exit 1; }
-[ -d ${ACMEDOMAINDIR} ] || { echo -e "${TIMESTAMP} - Domain directory does not exist" > ${LOGFILE}; exit 1; }
-[ -d ${ACMECHALLENGESDIR} ] || { echo -e "${TIMESTAMP} - Challenges directory does not exist" > ${LOGFILE}; exit 1; }
-[ -x ${PYTHONDIR}/python ] || { echo -e "${TIMESTAMP} - Binary python not executable" > ${LOGFILE}; exit 1; }
-
-#Get intermediate certificate
-curl  https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem \
-    > $ACMEDOMAINDIR/intermediate.pem || { echo -e "${TIMESTAMP} - Cannot get intermediate certificate" > ${LOGFILE}; exit 1; }
+[ -f $ACMEDIR/$ACME ] || { echo -e "${TIMESTAMP} - Binary acme-tiny not found" > ${LOGFILE}; exit 1; }
 
 #Generate new certificate(s)
 for DOMAIN in "${DOMAINS[@]}"
 do
     echo -e "${TIMESTAMP} - Generating certificate for ${DOMAIN}" > ${LOGFILE}
-    ${PYTHONDIR}/python ${ACMEDIR}/${ACMETINY} \
-        --account-key ${ACMESECRETSDIR}/${ACCOUNTKEY} \
-        --csr ${ACMEDOMAINDIR}/${DOMAIN}.csr \
-        --acme-dir ${ACMECHALLENGESDIR} \
-        > ${ACMEDOMAINDIR}/${DOMAIN}.crt 2> ${LOGFILE} || { echo -e "${TIMESTAMP} - Error occurs with renewing ${DOMAIN} certificate" > ${LOGFILE}; exit 1; }
-
-        cat ${ACMEDOMAINDIR}/${DOMAIN}.crt ${ACMEWORKDIR}/intermediate.pem > ${ACMEWORKDIR}/${DOMAIN}.pem
+    $ACMEDIR/$ACME --renew -d $DOMAIN 2> ${LOGFILE} || { echo -e "${TIMESTAMP} - Error occurs with renewing ${DOMAIN} certificate" > ${LOGFILE}; exit 1; }
 done
 
 
